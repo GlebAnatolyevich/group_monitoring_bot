ddd3b393fa7ab3e7afd08cbca7eabea16b9bda47
rubin <gleb.shaxvorostov@mail.ru>
2024-10-23 05:49:29 +0300

    доп

---
+++ main.py
@@ -0,0 +1,19 @@
+#from bot.bot import Bot
+#from bot.handler import MessageHandler
+#from token import TOKEN
+from config import tokenbot
+
+#TELEGRAM_TOKEN = os.getenv('token')
+
+
+print(tokenbot+"11")
+#TOKEN = TOKEN.token #your token herep
+
+#bot = Bot(token=TOKEN)
+
+#def message_cb(bot, event):
+#    bot.send_text(chat_id=event.from_chat, text=event.text)
+
+#bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
+#bot.start_polling()
+#bot.idle()
