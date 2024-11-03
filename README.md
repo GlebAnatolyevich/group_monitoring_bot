d9ab5ffbe0bfd97402b9cc88ab70905b3f03b055
rubin <gleb.shaxvorostov@mail.ru>
2024-11-03 12:33:18 +0300

    neew

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ group_monitoring_bot
 Описание структуры
 ## VENV
 source ./botvenv/bin/activate
+
+
+## Для подкления к базе из личных источников нужно в pg_hba.conf указать свой ip как доверенный 
+
+host    all             all             ххх.ххх.ххх.ххх/32       scram-sha-256
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 #from bot.bot import Bot
 #from bot.handler import MessageHandler
 #from token import TOKEN
-from config import tokenbot
+#from config import tokenbot
 
 #TELEGRAM_TOKEN = os.getenv('token')
+print("fff")
 
-
-print(tokenbot+"11")
-#TOKEN = TOKEN.token #your token herep
-
-#bot = Bot(token=TOKEN)
+#print(tokenbot+"11")
+#bot = Bot(token=tokenbot)
 
 #def message_cb(bot, event):
 #    bot.send_text(chat_id=event.from_chat, text=event.text)
@@ -17,3 +15,20 @@ print(tokenbot+"11")
 #bot.dispatcher.add_handler(MessageHandler(callback=message_cb))
 #bot.start_polling()
 #bot.idle()
+
+
+import psycopg2
+conn = psycopg2.connect(dbname='monbd', user='botpg', password='=QCciL5Z1g42ppU4M', host='46.8.71.147')
+cursor = conn.cursor()
+
+
+cursor.execute('SELECT * FROM themes')
+records = cursor.fetchall()
+
+print(records)
+
+
+
+
+cursor.close()
+conn.close()
